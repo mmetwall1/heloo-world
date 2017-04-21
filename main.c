eaf6e7ef4efb7dc1e08ae27f4388b480bb15492a
mmetwall1 <en.mmeg@gmail.com>
2017-04-21 14:07:28 +0200

    add debug option

--- main.c
+++ main.c
@@ -1,46 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	int x = 0;
-	int y;
-	int z;
-	printf("%d", x);
-	printf("%d", y);
-	printf("%d", z);
-	printf("hello world");
-#if x==1
-	y =z;
+	int x, y, z;
+	x = y = z = 0;
+#if DEBUG == ON
+	printf("%d, %d, %d", x, y, z);
 #endif
 	return 0;
 }
 
 void testFun(void)
 {
-	return 0;
+	return;
 }
 
 void Fun1(void)
 {
-	return 0;
+	return;
 }
 
 void Fun2(void)
 {
-	return 0;
+	return;
 }
 
 void Fun3(void)
 {
-	return 0;
+	return;
 }
 
 void Fun4(void)
 {
-	return 0;
+	return;
 }
 
 void Fun5(void)
 {
-	return 0;
+	return;
 }
